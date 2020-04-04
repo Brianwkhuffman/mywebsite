5a95f70c973bc33f0fe20b4cce7d43d5dff39776
bri <brianwkhuffman@gmail.com>
2020-04-03 19:21:00 -1000

    Updates

---
+++ precache-manifest.01d14b7ba61ad9cfa010c9f3f22a191c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d2b83ead9d254d84dfce18ad078c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c836c02a43689d3c04",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c55a11276482d99bf3d0",
+    "url": "/static/js/2.ef35e62e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef35e62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c836c02a43689d3c04",
+    "url": "/static/js/main.86ccd3e0.chunk.js"
+  },
+  {
+    "revision": "8444ae0e1377649995f2",
+    "url": "/static/js/runtime-main.bc8e0b89.js"
+  }
+]);
