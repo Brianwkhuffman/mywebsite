b45b08b1fb8ceeb2eac636c3a11892fee222fc79
bri <brianwkhuffman@gmail.com>
2020-10-15 10:47:47 -1000

    removes skill bar

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,19 +4,19 @@ import placeholder from '../placeholder.jpg';
 class Portfolio extends Component {
   render() {
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
         // var projectImage = projects.image;
         return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
+          <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={placeholder} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
+              <img alt={projects.title} src={placeholder} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
+              </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
@@ -27,18 +27,18 @@ class Portfolio extends Component {
     return (
       <section id="portfolio">
 
-      <div className="row">
+        <div className="row">
 
-         <div className="twelve columns collapsed">
+          <div className="twelve columns collapsed">
 
-            <h1>Projects!</h1>
+            <h1>Projects</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+              {projects}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
